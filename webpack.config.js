e430c124dadb69591a5166c069ef1075db272da9
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2019-02-27 14:25:33 -0300

    finished different start scripts for css-live changes and js live-changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,4 @@
 const path = require('path');
-/* const ASSET_PATH = '/assets';
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
- */
-/* __webpack_public_path__ = '/assets'; */
 
 module.exports = {
   entry: path.resolve(__dirname, 'app/js'),
@@ -16,33 +12,13 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }/*,
-      { 
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-          }
-        ]
-      } */
+      }
     ]
   },
-  /* plugins: [
-    new ExtractTextPlugin('build/index.css'),
-    new webpack.DefinePlugin({
-      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
-    })
-  ], */
   devServer: {
     port: 9000,
     contentBase: path.resolve(__dirname, 'build')
-  }/* ,
-  plugins: [
-    // This makes it possible for us to safely use env vars on our code
-    new webpack.DefinePlugin({
-      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
-    })
-  ] */
+  }
 }
 
 
